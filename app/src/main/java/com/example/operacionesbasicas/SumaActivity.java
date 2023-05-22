c2c911527b9b85c99870dd16288d3328ec96ae3b
PamelaVargas2000 <ana.vargas794@ist17dejulio.edu.ec>
2023-05-22 18:06:30 -0500

    OPERACION SUMA

---
+++ app/src/main/java/com/example/operacionesbasicas/SumaActivity.java
@@ -0,0 +1,27 @@
+package com.example.operacionesbasicas;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+
+public class SumaActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_suma);
+    }
+public void RegreasarMenu(View view){
+        this.finish();
+
+}
+public void sumar(View view){
+    EditText numero1= (EditText) findViewById(R.id.editTextNumber1);
+    EditText numero2= (EditText) findViewById(R.id.editTextNumber2);
+    TextView total= findViewById(R.id.txtEResultado);
+    Double.parseDouble(numero1.getText().toString())+Double.parseDouble(numero2.getText().toString());
+
+}
+}
