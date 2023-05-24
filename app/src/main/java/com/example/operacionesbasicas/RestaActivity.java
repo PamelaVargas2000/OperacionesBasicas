5f8105bfb3e270af1a78da208def4891a11f773b
PamelaVargas2000 <ana.vargas794@ist17dejulio.edu.ec>
2023-05-24 12:31:30 -0500

    programar el boton resta y realizar la operacion en RestaActivity

--- app/src/main/java/com/example/operacionesbasicas/RestaActivity.java
+++ app/src/main/java/com/example/operacionesbasicas/RestaActivity.java
@@ -3,6 +3,9 @@ package com.example.operacionesbasicas;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class RestaActivity extends AppCompatActivity {
 
@@ -10,5 +13,20 @@ public class RestaActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_resta2);
+    }
+    public void RegreasarMenu(View view) {
+        this.finish();
+
+    }
+
+    public void restar(View view) {
+        EditText numero1 = (EditText) findViewById(R.id.txtRestaNum1);
+        EditText numero2 = (EditText) findViewById(R.id.txtRestaNum2);
+        TextView total = findViewById(R.id.txtEResultado2);
+
+
+        double resultado = Double.parseDouble(numero1.getText().toString()) - Double.parseDouble(numero2.getText().toString());
+        total.setText(resultado + "");
+
     }
 }
