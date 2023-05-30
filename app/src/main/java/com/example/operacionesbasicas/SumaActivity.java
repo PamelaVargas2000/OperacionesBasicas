49e44f10a3d352da130094d83b3ff9b1d943199f
PamelaVargas2000 <ana.vargas794@ist17dejulio.edu.ec>
2023-05-29 22:13:30 -0500

    Realizar un meodo startSpeechtoText

--- app/src/main/java/com/example/operacionesbasicas/SumaActivity.java
+++ app/src/main/java/com/example/operacionesbasicas/SumaActivity.java
@@ -49,6 +49,20 @@ public class SumaActivity extends AppCompatActivity {
         });
     }
 
+    private void startSpeechToText() {
+        if (permissionToRecordAccepted) {
+            Intent intent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
+            intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL, RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
+            intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE, "en-US");
+            intent.putExtra(RecognizerIntent.EXTRA_PROMPT, "Speak something...");
+
+            startActivityForResult(intent, REQUEST_RECORD_AUDIO_PERMISSION);
+        } else {
+            Toast.makeText(this, "Permission to record audio not granted", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+
     public void RegreasarMenu(View view) {
         this.finish();
 
